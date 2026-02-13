cb7b0db21ec746547a54717dad49ef69fe28bfb7
Lucas Ywamoto <92839243+lucasywamoto@users.noreply.github.com>
2026-02-13 11:18:03 -0500

    Add Hashids integration for room code generation and update appsettings

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,5 +1,6 @@
 
 using backend.Data;
+using HashidsNet;
 using Microsoft.EntityFrameworkCore;
 
 namespace backend;
@@ -11,8 +12,11 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         builder.Services.AddSignalR();
+
         builder.Services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+        builder.Services.AddSingleton(new Hashids(builder.Configuration["HashSalt:Value"] ?? throw new InvalidOperationException("HashSalt not found in configuration"), 5, "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"));
 
         var app = builder.Build();
 
--- backend/Services/RoomService.cs
+++ backend/Services/RoomService.cs
@@ -1,13 +1,39 @@
 using System;
+using backend.Data;
 using backend.Models;
+using HashidsNet;
 
 namespace backend.Services;
 
-public class RoomService : IRoomService
+public class RoomService(ApplicationDbContext context, Hashids hashids) : IRoomService
 {
+    readonly ApplicationDbContext _context = context;
+    readonly Hashids _hashids = hashids;
+
+
     public Task<string> CreateRoom()
     {
-        throw new NotImplementedException();
+        Room room = new()
+        {
+            CreatedAt = DateTime.UtcNow,
+            ExpiresAt = DateTime.UtcNow.AddDays(7)
+        };
+
+        _context.Rooms.Add(room);
+        _context.SaveChanges();
+
+        string code = _hashids.Encode(room.Id);
+
+        if (code.Length > 5)
+        {
+            throw new InvalidOperationException($"Generated code exceeds 5 characters: {code}");
+        }
+
+        room.Code = code;
+
+        _context.SaveChanges();
+
+        return Task.FromResult(room.Code);
     }
 
     public string Decrypt(string cipherText)
